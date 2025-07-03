f9c94e1ea03a44b877b039ea7c862410810b3ea9
ankitgujre <kumarankit65494@gmail.com>
2025-07-03 13:40:46 +0530

    second code

--- .history/HelloWorld_20250627122359.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("Hello World! This is my first MCA Java program.");
-        System.out.println("I am excited to learn Java programming for College.");
-    }
-}
---
+++ SecondCode.java
@@ -0,0 +1,6 @@
+public class SecondCode{
+    public static void main(String[] args){
+        System.out.println("ankit");
+        System.out.print('a');
+    }
+}
